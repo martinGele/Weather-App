aa21c73325ae6e0423cdfe558980bfb00f5d1bd6
martingelevski <martin.gelevski@codewell.mk>
2019-12-12 13:57:44 +0100

    Showing list of cities

--- app/src/main/java/com/martin/one/ui/detail/DetailScreenFragment.kt
+++ app/src/main/java/com/martin/one/ui/detail/DetailScreenFragment.kt
@@ -6,20 +6,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.example.github.di.Injectable
 import com.martin.one.R
+import com.martin.one.adapter.CitiesAdapter
+import com.martin.one.databinding.FragmentDetailScreenBinding
+import com.martin.weatherestonia.model.Forecast
+import kotlinx.android.synthetic.main.fragment_detail_screen.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class DetailScreenFragment : Fragment() {
+class DetailScreenFragment : Fragment(), Injectable {
+
+    var weather: Forecast? = null
+
+
+    private val listAdapterCities = CitiesAdapter(arrayListOf())
+    lateinit var dataBinding: FragmentDetailScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_screen, container, false)
+
+
+        dataBinding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_detail_screen,
+                container,
+                false
+            )
+        return dataBinding.root
+
+
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let { weather = DetailScreenFragmentArgs.fromBundle(it).Forecast }
+        dataBinding.weather = weather
+
+        dataBinding.weather?.day?.places?.let { listAdapterCities.updateCitiesList(it) }
+
+        recycleViewCities.apply {
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = listAdapterCities
+
+        }
+
+
+    }
 
 }
